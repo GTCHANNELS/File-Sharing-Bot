e46f935cb767900a8ac98cf92c546d0be03f519c
GTCHANNELS <108328260+GTCHANNELS@users.noreply.github.com>
2024-03-07 14:32:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -77,10 +77,17 @@ async def start_command(client: Client, message: Message):
 
             try:
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                ab = await msg.copy()
+asyncio.create_task(func(ab, 600))
                 await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                import asyncio
+
+async def func(msg, time_in_seconds):
+    await asyncio.sleep(time_in_seconds)
+    await msg.delete()
             except:
                 pass
         return
